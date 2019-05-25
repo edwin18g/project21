392b6f9fa04eb7e3863307cb58c8618bba38a442
edwin18g <edwin18g@gmail.com>
2019-05-25 17:24:37 +0000

    new

--- application/config/development/config.php
+++ application/config/development/config.php
@@ -23,4 +23,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://classeventie.test/';
+$config['base_url'] = 'http://zeyborn.edwin18g.com/';
